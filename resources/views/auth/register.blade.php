5850316c4b7570b88ae31eebb3a704cf165c1897
jellyfish <thomas.lawanson@gmail.com>
2017-05-18 00:32:14 +0100

    style

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,9 +3,13 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+        <div class="col-md-6 col-md-offset-3">
             <div class="panel panel-default">
-                <div class="panel-heading">Register</div>
+              <div class="panel-body">Register</div>
+            </div>
+
+            <div class="panel panel-default">
+              <br />
                 <div class="panel-body">
                     <form class="form-horizontal" role="form" method="POST" action="{{ route('register') }}">
                         {{ csrf_field() }}
@@ -62,8 +66,8 @@
                         </div>
 
                         <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-secondary">
+                            <div class="col-md-6 col-md-offset-3">
+                                <button type="submit" class="btn btn-primary btn-block">
                                     Register
                                 </button>
                             </div>
